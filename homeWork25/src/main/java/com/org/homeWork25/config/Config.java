263b4904b5e19e2b7b17e36440464b04ea8cc604
reteruvsky <thebingolo@gmail.com>
2023-05-13 04:24:16 +0300

    fixed config

--- homeWork25/src/main/java/com/org/homeWork25/config/Config.java
+++ homeWork25/src/main/java/com/org/homeWork25/config/Config.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.org.homeWork25")
 public class Config {
 
 }
--- homeWork25/src/main/java/com/org/homeWork25/service/impl/OrderServiceImpl.java
+++ homeWork25/src/main/java/com/org/homeWork25/service/impl/OrderServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @Slf4j
-public class OrderServiceImpl implements OrderService {
+public class OrderServiceImpl {
     @Autowired
     private OrderRepository orderRepository;
 
